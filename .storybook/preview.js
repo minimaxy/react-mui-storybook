09a4b24b57734ab5afebf7bae581dd7776bdc444
minim <minimarisuto@gmail.com>
2023-01-30 00:48:37 +0900

    Fiurst

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/react";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+];
